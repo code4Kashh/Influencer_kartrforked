b2b9b3b9467a1857feb12e83977d4f63ed7831d1
Kashish <kashishgarg594@gmail.com>
2025-04-28 00:40:01 +0530

    Changed clone path to local directory

--- Influencer_kartr/virtual_influencer.py
+++ Influencer_kartr/virtual_influencer.py
@@ -1,14 +1,14 @@
-        # Install the required version of pyttsx3
-        !pip install pyttsx3==2.91.2
+# Install the required version of pyttsx3
+!pip install pyttsx3==2.91.2
+!pip install pygit2==1.12.2
 
-        # Change directory to /content
-        %cd /content
+# Change directory to /content
+%cd /content
 
-        # Clone the Fooocus repository
-        !git clone https://github.com/lllyasviel/Fooocus.git
+# (No need to clone anymore)
 
-        # Change into the Fooocus directory
-        %cd Fooocus
+# Change into the Fooocus directory (local path)
+%cd Fooocus-main/Fooocus-main
 
-        # Run the entry_with_update.py script with the --share flag
-        !python entry_with_update.py --share
+# Run the entry_with_update.py script with the --share flag
+!python entry_with_update.py --share
